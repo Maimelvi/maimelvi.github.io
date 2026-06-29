32cc3bca7fa65bfedc4b6cda6cb18126ca0e40ed
Maiara Nascimento <mns.melvi@gmail.com>
2026-06-29 20:29:13 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuHamburguer = document.querySelector('.menu-hamburguer');
-const nav = document.querySelector('.nav-responsive');
-
-menuHamburguer.addEventListener('click', () => {
-    menuHamburguer.classList.toggle('change');
-    nav.classList.toggle('active');
-});
